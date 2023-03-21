6e5f458f1ad09ebcd94fdb31ce87f1d19e25f787
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 00:29:20 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-DB storage
+"""DB storage
 """
 import models
 from models.base_model import BaseModel, Base
@@ -17,73 +16,64 @@ HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
 
 
 class DBStorage:
-    """
-    database storage for MySQL conversion
+    """database storage for mysql conversion
     """
     __engine = None
     __session = None
 
     def __init__(self):
-        """
-        Initializer for DBStorage
-        """
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(HBNB_MYSQL_USER,
-                                              HBNB_MYSQL_PWD,
-                                              HBNB_MYSQL_HOST,
-                                              HBNB_MYSQL_DB),
+        """initializer for DBStorage"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB),
                                       pool_pre_ping=True)
-        env = getenv('HBNB_ENV')
-        if env == 'test':
+        env = getenv("HBNB_ENV")
+        if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """
-        Query the current session and list all instances of cls
+        """Query the current session and list all instances of cls
         """
         result = {}
         if cls:
             for row in self.__session.query(cls).all():
-                key = '{}.{}'.format(cls.__name__, row.id)
+                key = "{}.{}".format(cls.__name__, row.id)
                 row.to_dict()
                 result.update({key: row})
         else:
             for table in models.dummy_tables:
                 cls = models.dummy_tables[table]
                 for row in self.__session.query(cls).all():
-                    key = '{}.{}'.format(cls.__name__, row.id)
+                    key = "{}.{}".format(cls.__name__, row.id)
                     row.to_dict()
                     result.update({key: row})
         return result
 
     def rollback(self):
-        """
-        Rollback changes
+        """rollback changes
         """
         self.__session.rollback()
 
     def new(self, obj):
-        """
-        Add object to current session
+        """add object to current session
         """
         self.__session.add(obj)
 
     def save(self):
-        """
-        Commit current done work
+        """commit current done work
         """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        Delete obj from session
+        """delete obj from session
         """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """
-        Reload the session
+        """reload the session
         """
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
@@ -91,9 +81,7 @@ class DBStorage:
         self.__session = Scope()
 
     def close(self):
-        """
-        Close the session
+        """display our HBNB data
         """
         self.__session.close()
         self.reload()
-
