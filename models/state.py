abcd55026c3af3ad1925d453f0edeba28690a853
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 00:30:44 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-
 """
-State class
+    contains state class to represent a state
 """
 
-import os
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """
-    This is the class for State.
-
-    Attributes:
-        name (str): input name
-    """
-    __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City",
-                              cascade="all, delete, delete-orphan",
-                              backref="state")
+    """ State class: class to represent states of cities"""
+    if (storage_engine == 'db'):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
+        name = ""
+
         @property
         def cities(self):
-            """Return a list of cities."""
-            city_list = [city for city in models.storage.all(City).values()
-                         if city.state_id == self.id]
-            return city_list
+            """cities list
+            """
+            result = []
+            for j, i in models.storage.all(models.city.City).items():
+                if (i.state_id == self.id):
+                    result.append(i)
+            return result
