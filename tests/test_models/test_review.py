7c31e109d8b32dd213ab27cead051813b9c30509
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 14:40:39 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
+"""
+    contains review class to represent reviews
+"""
+from models.base_model import BaseModel, Base
+from models.place import Place
+from models.user import User
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
-class test_review(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
-
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
-
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+class Review(BaseModel, Base):
+    """
+        Review class
+    """
+    if (storage_engine == 'db'):
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"))
+        user_id = Column(String(60), ForeignKey("users.id"))
+        text = Column(String(1024), nullable=False)
+        place = relationship("Place", back_populates="reviews")
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
