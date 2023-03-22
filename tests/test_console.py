a993720dda7565d65d977ef9542c163057d3e2ae
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 16:19:42 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,11 +1,9 @@
-
 #!/usr/bin/python3
 """test for console to make it start working"""
 import unittest
 from io import StringIO
 from console import HBNBCommand
 import sys
-from unittest.mock import patch
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -35,15 +33,28 @@ class TestConsole(unittest.TestCase):
         temp_out = StringIO()
         sys.stdout = temp_out
         return temp_out.getvalue()
-    
-    def test_create_error(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().do_create('')
-            self.assertEqual(f.getvalue(), "** class name missing **\n")
 
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().do_create(None)
-            self.assertEqual(f.getvalue(), "** class name missing **\n")
+    def test_create_error(self):
+        """test if create works right"""
+        temp_out = StringIO()
+        sys.stdout = temp_out
+
+        HBNBCommand().do_create(None)
+        self.assertEqual(temp_out.getvalue(), '** class name missing **\n')
+        temp_out.close()
+
+        temp_out = StringIO()
+        sys.stdout = temp_out
+        HBNBCommand().do_create("base")
+        self.assertEqual(temp_out.getvalue(), '** class doesn\'t exist **\n')
+        temp_out.close()
+
+        temp_out = StringIO()
+        sys.stdout = temp_out
+        HBNBCommand().do_create("BaseModel")
+        self.assertEqual(temp_out.getvalue(), '** class doesn\'t exist **\n')
+        temp_out.close()
+        sys.stdout = sys.__stdout__
 
 if __name__ == "__main__":
     unittest.main()
