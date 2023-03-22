46fd2e1379cdac8b2eb925b021114936158a9758
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 12:15:17 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,6 @@
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.user import User
-from models.place import place_amenity
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from os import environ
