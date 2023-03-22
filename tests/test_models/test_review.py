46c4eef49ea039f91809ae19bcd70bb1b6d23ba3
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-03-22 14:41:50 +0200

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,51 @@
 #!/usr/bin/python3
 """
-    contains review class to represent reviews
+    test reviews
 """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.review import Review
 from models.place import Place
 from models.user import User
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.sql.schema import ForeignKey
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
+import unittest
 
 
-class Review(BaseModel, Base):
+class test_Review(unittest.TestCase):
     """
-        Review class
+        test for Review class
     """
-    if (storage_engine == 'db'):
-        __tablename__ = "reviews"
-        place_id = Column(String(60), ForeignKey("places.id"))
-        user_id = Column(String(60), ForeignKey("users.id"))
-        text = Column(String(1024), nullable=False)
-        place = relationship("Place", back_populates="reviews")
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    @classmethod
+    def setUpClass(cls):
+        """
+            setup
+        """
+        cls.dummy_review = Review()
+        cls.dummy_review.text = "test"
+        cls.dummy_review.user_id = User().id
+        cls.dummy_review.place_id = Place().id
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+            tear down
+        """
+        del cls.dummy_review
+
+    def test_inheritance(self):
+        """
+            test proper inheritance
+        """
+        self.assertIsInstance(self.dummy_review, BaseModel)
+        self.assertTrue(hasattr(self.dummy_review, "id"))
+        self.assertTrue(hasattr(self.dummy_review, "created_at"))
+        self.assertTrue(hasattr(self.dummy_review, "updated_at"))
+
+    def test_attrs(self):
+        """
+            test attributes
+        """
+        self.assertTrue(hasattr(self.dummy_review, "text"))
+        self.assertTrue(hasattr(self.dummy_review, "user_id"))
+        self.assertTrue(hasattr(self.dummy_review, "place_id"))
+
+if __name__ == "__main__":
+    unittest.main()
